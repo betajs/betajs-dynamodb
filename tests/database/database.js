ee93a52ff95b027a46350bc0207eb4b9422e3d8b
Oliver Friedmann <oliver.friedmann@gmail.com>
2020-06-05 00:35:09 -0400

    Test Global Index

--- tests/database/database.js
+++ tests/database/database.js
@@ -69,7 +69,6 @@ QUnit.test("dynamo hash range local index", function (assert) {
     const table = db.getTable("TableTest", { primary: { hash: "id", range: "date" }, locals: ["priority"], attributes: { id: "S", date: "S", priority: "S" } });
     table.deleteTable().callback(function () {
         table.createTable().success(function (result) {
-            console.log("asdf");
             table.insertRow({
                 "id": "foobar",
                 "date": "1",
@@ -85,6 +84,45 @@ QUnit.test("dynamo hash range local index", function (assert) {
                     table.find({"id": "foobar", "priority": {"$gte": "A"}}).success(function (items) {
                         table.updateRow({"id": "foobar", "date": "1"}, {"bar": "foo"}).success(function () {
                             table.findOne({"id": "foobar", "priority": "B"}).success(function (result) {
+                                assert.equal(result.bar, "foo");
+                                table.removeRow({"id": "foobar", "date": "1"}).success(function () {
+                                    table.findOne({"id": "foobar", "date": "1"}).success(function (result) {
+                                        assert.equal(result, null);
+                                        db.destroy();
+                                        done();
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+
+
+QUnit.test("dynamo hash global index", function (assert) {
+    const done = assert.async();
+    const db = new BetaJS.Data.Databases.DynamoDB.DynamoDatabase({ endpoint: "http://localhost:8000",  region: "eu-west-1" });
+    const table = db.getTable("TableTest", { primary: { hash: "id" }, globals: [{hash: "date", range: "priority"}], attributes: { id: "S", date: "S", priority: "S" } });
+    table.deleteTable().callback(function () {
+        table.createTable().success(function (result) {
+            table.insertRow({
+                "id": "foobar",
+                "date": "1",
+                "bar": "baz",
+                "priority": "B"
+            }).success(function () {
+                table.insertRow({
+                    "id": "foobaz",
+                    "date": "2",
+                    "bar": "bazinga",
+                    "priority": "A"
+                }).success(function () {
+                    table.find({"date": "2", "priority": {"$gte": "A"}}).success(function (items) {
+                        table.updateRow({"id": "foobar", "date": "1"}, {"bar": "foo"}).success(function () {
+                            table.findOne({"date": "1", "priority": "B"}).success(function (result) {
                                 assert.equal(result.bar, "foo");
                                 table.removeRow({"id": "foobar", "date": "1"}).success(function () {
                                     table.findOne({"id": "foobar", "date": "1"}).success(function (result) {
